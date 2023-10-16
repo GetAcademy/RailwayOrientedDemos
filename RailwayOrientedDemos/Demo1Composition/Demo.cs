870b927a7de3063d9e3355babe1b962e393d8232
GET Academy <terje@getacademy.no>
2023-10-16 15:40:42 +0200

    x

---
+++ RailwayOrientedDemos/Demo1Composition/Demo.cs
@@ -0,0 +1,12 @@
+﻿namespace RailwayOrientedDemos.Demo1Composition
+{
+    internal class Demo
+    {
+        public static void Run()
+        {
+            Func<Apple, Banana> bananaFromApple = apple => new Banana();
+            Func<Banana, Cherry> cherryFromBanana = banana => new Cherry();
+            Func<Apple, Cherry> cherryFromApple = apple => cherryFromBanana(bananaFromApple(apple));
+        }
+    }
+}
