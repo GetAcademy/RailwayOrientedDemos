7f2506fd09f53770c4edafa1590a1b675ad58883
GET Academy <terje@getacademy.no>
2023-10-16 16:02:52 +0200

    x

--- RailwayOrientedDemos/Demo1Result/Result.cs
+++ RailwayOrientedDemos/Demo1Result/Result.cs
@@ -6,12 +6,22 @@
 
     class Success<T> : Result<T>
     {
+        public Success(T result)
+        {
+            Result = result;
+        }
+
         public T Result;
     }
 
     class Failure<T> : Result<T>
     //class Failure : Result
     {
+        public Failure(string errorMessage)
+        {
+            ErrorMessage = errorMessage;
+        }
+
         public string ErrorMessage;
     }
 }
---
+++ RailwayOrientedDemos/Demo3Adapt/Demo.cs
@@ -0,0 +1,25 @@
+﻿using RailwayOrientedDemos.Demo1Result;
+
+namespace RailwayOrientedDemos.Demo3Adapt
+{
+    internal class Demo
+    {
+        public static void Run()
+        {
+            Func<int, Result<int>> divide1000byN =
+                n => n == 0
+                    ? new Failure<int>("Kan ikke dele på 0")
+                    : new Success<int>(1000 / n);
+
+            Func<
+                Func<       int,  Result<int>>,
+                Func<Result<int>, Result<int>>> adapt =
+                oneTrackInputFunction => 
+                    twoTrackInput => 
+                        twoTrackInput is Success<int> input ? oneTrackInputFunction(input.Result) 
+                            : null!;
+
+            var twoTrackInputVersionOfDivideByN = adapt(divide1000byN);
+        }
+    }
+}
